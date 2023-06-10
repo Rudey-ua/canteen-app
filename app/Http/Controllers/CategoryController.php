11bf0c53e8e4737bb50d36ed1310ac1bedff6f07
Maksym-Kostenko-Leasoft <maksym.kostenko1@nure.ua>
2023-06-10 22:04:27 +0300

    Add CRUD to categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -34,18 +35,43 @@ class CategoryController extends Controller
         ], 200);
     }
 
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
+        $categories = Category::create([
+            "name" => $request->name,
+        ]);
 
+        return response()->json([
+            "status" => true,
+            "categories" => new CategoryResource($categories)
+        ], 201)->setStatusCode(201, 'Categories created successfully!');
     }
 
-    public function update($id, Request $request)
+    public function update(StoreCategoryRequest $request, $id)
     {
+        $categories = Category::find($id);
 
+        if (!$categories) {
+            return response()->json([
+                "status" => false,
+                "message" => "Categories not found!"
+            ], 404)->setStatusCode(404, 'Categories not found!');
+        }
+
+        $categories->name = $request->name;
+        $categories->save();
+
+        return response()->json([
+            "status" => true,
+            "category" => new CategoryResource($categories)
+        ], 200)->setStatusCode(200, 'Categories data is updated!');
     }
 
     public function destroy($id)
     {
+        $categories = Category::findOrFail($id);
+        $categories->delete();
 
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -37,8 +37,11 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+});
 
 /*Categories END*/
