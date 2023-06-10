2b4a7ca7da3d3926dc9097b8b5fd5df242928289
Maksym-Kostenko-Leasoft <maksym.kostenko1@nure.ua>
2023-06-10 17:01:25 +0300

    Add Order factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Dish;
+use App\Models\Order;
 use App\Models\Reservation;
 use App\Models\Restaurant;
 use App\Models\Table;
@@ -42,5 +43,7 @@ class DatabaseSeeder extends Seeder
         Dish::factory()->count(10)->create();
 
         Reservation::factory()->count(5)->create();
+
+        Order::factory()->count(2)->create();
     }
 }
