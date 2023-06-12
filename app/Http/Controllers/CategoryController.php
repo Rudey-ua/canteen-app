b7969ef88b88a06aee690fd9d095c634c45ec1f3
Maksym-Kostenko-Leasoft <maksym.kostenko1@nure.ua>
2023-06-12 19:00:59 +0300

    Made refactoring DishController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,84 +5,49 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Resources\Category as CategoryResource;
 use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    public function index(): \Illuminate\Http\JsonResponse
+    public function index(): JsonResponse
     {
         $categories = Category::all();
 
         return response()->json([
-            "status" => true,
             "categories" => new CategoryCollection($categories)
-        ], 200)->setStatusCode(200, 'The resource has been fetched and transmitted in the message body.');
+        ]);
     }
 
-    public function show($id): \Illuminate\Http\JsonResponse
+    public function show($id): JsonResponse
     {
-        $categories = Category::find($id);
+        $categories = Category::findOrFail($id);
 
-        if(!$categories) return response()->json([
-            "status" => false,
-            "message" => "Categories not found!"
-        ], 404)->setStatusCode(404, 'Categories not found!');
-
-        return response()->json([
-            "status" => true,
-            "category" => new CategoryResource($categories)
-        ], 200);
+        return response()->json(new CategoryResource($categories));
     }
 
-    public function store(StoreCategoryRequest $request): \Illuminate\Http\JsonResponse
+    public function store(StoreCategoryRequest $request): JsonResponse
     {
         $categories = Category::create([
             "name" => $request->name,
         ]);
 
-        return response()->json([
-            "status" => true,
-            "categories" => new CategoryResource($categories)
-        ], 201)->setStatusCode(201, 'Categories created successfully!');
+        return response()->json(new CategoryResource($categories), 201);
     }
 
-    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    public function update(StoreCategoryRequest $request, $id): JsonResponse
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'sometimes|string|max:255',
-        ]);
+        $categories = Category::findOrFail($id);
 
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false,
-                'errors' => $validator->errors(),
-            ], 422)->setStatusCode(422, 'Validation error');
-        }
-
-        $categories = Category::find($id);
-
-        if (!$categories) {
-            return response()->json([
-                "status" => false,
-                "message" => "Categories not found!"
-            ], 404)->setStatusCode(404, 'Categories not found!');
-        }
-
-        if ($request->has('name')) {
-            $categories->name = $request->name;
-        }
-
+        $categories->name = $request->name;
         $categories->save();
 
-        return response()->json([
-            "status" => true,
-            "category" => new CategoryResource($categories)
-        ], 200)->setStatusCode(200, 'Categories data is updated!');
+        return response()->json(new CategoryResource($categories));
     }
 
-    public function destroy($id): \Illuminate\Http\JsonResponse
+    public function destroy($id): JsonResponse
     {
         $categories = Category::findOrFail($id);
         $categories->delete();
--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreDishRequest;
+use App\Http\Requests\UpdateDishRequest;
 use App\Http\Resources\DishCollection;
 use App\Models\Dish;
 use App\Http\Resources\Dish as DishResource;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class DishController extends Controller
 {
-    public function index(): \Illuminate\Http\JsonResponse
+    public function index(): JsonResponse
     {
         $dishes = Dish::all();
 
@@ -20,64 +20,30 @@ class DishController extends Controller
         ]);
     }
 
-    public function show($id): \Illuminate\Http\JsonResponse
+    public function show($id): JsonResponse
     {
         $dishes = Dish::findOrFail($id);
 
         return response()->json(new DishResource($dishes));
     }
 
-    public function store(StoreDishRequest $request): \Illuminate\Http\JsonResponse
+    public function store(StoreDishRequest $request): JsonResponse
     {
-        $dishes = Dish::create([
-            "name" => $request->name,
-            "description" => $request->description,
-            "price" => $request->price,
-            "category_id" => $request->category_id,
-            "ingredients" => $request->ingredients,
-            "special_requirements" => $request->special_requirements,
-            "recipe" => $request->recipe,
-            "restaurant_id" => $request->restaurant_id,
-        ]);
+        $dishes = Dish::create($request->validated());
 
         return response()->json(new DishResource($dishes), 201);
     }
 
-    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    public function update(UpdateDishRequest $request, $id): JsonResponse
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'sometimes|string|max:255',
-            'description' => 'sometimes|string',
-            'price' => 'sometimes|numeric',
-            'category_id' => 'sometimes|exists:categories,id',
-            'ingredients' => 'sometimes|string',
-            'special_requirements' => 'nullable|string',
-            'recipe' => 'nullable|string',
-            'restaurant_id' => 'sometimes|exists:restaurants,id',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false,
-                'errors' => $validator->errors(),
-            ], 422)->setStatusCode(422, 'Validation error');
-        }
-
         $dish = Dish::findOrFail($id);
 
-        $fieldsToUpdate = ['name', 'description', 'price', 'category_id', 'ingredients', 'special_requirements', 'recipe', 'restaurant_id'];
-
-        foreach ($fieldsToUpdate as $field) {
-            if ($request->has($field)) {
-                $dish->$field = $request->$field;
-            }
-        }
-        $dish->save();
+        $dish->update($request->validated());
 
         return response()->json(new DishResource($dish));
     }
 
-    public function destroy($id): \Illuminate\Http\JsonResponse
+    public function destroy($id): JsonResponse
     {
         Dish::findOrFail($id)->delete();
 
