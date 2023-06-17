b6d58950332a10ebcd7d59c10a2ef6ff3e65d51c
Maksym-Kostenko-Leasoft <maksym.kostenko1@nure.ua>
2023-06-17 20:32:55 +0300

    Clean the code

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,15 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Order\StoreOrderRequest;
 use App\Http\Resources\Order\Order as OrderResource;
 use App\Http\Resources\Order\OrderCollection;
-use App\Models\Dish;
-use App\Models\Payment;
 use App\Models\Order;
-use App\Models\Reservation;
-use App\Models\Table;
 use App\Services\OrderService;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 use App\Http\Resources\Payment\Payment as PaymentResource;
 
 class OrderController extends Controller
@@ -34,18 +29,14 @@ class OrderController extends Controller
 
     public function store(StoreOrderRequest $request): JsonResponse
     {
-        $validated = $request->validated();
-
         try {
-            $order = OrderService::createOrder($validated);
+            $order = OrderService::createOrder($request->validated());
+            $payment = OrderService::createPayment($order, $request->validated());
         } catch (Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
 
-        $payment = OrderService::createPayment($order, $validated);
-
         return response()->json([
-            'message' => 'Order created successfully',
             'order' => new OrderResource($order),
             'payment' => new PaymentResource($payment)
         ], 201);
