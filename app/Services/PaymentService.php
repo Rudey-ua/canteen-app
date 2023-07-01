76b9291c43a1be16615ba5b5cbc98b5ecbdc87d1
Maksym-Kostenko-Leasoft <maxim.kostenko@leasoft.org>
2023-07-01 20:05:35 +0300

    Fix bug you know

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -11,11 +11,11 @@ class PaymentService
 {
     public function findOrder(Table $table)
     {
-        $reservation = Reservation::where('table_id', $table->id)->get();
+        $reservation = Reservation::where('table_id', $table->id)
+            ->where('status', '!=', 'paid')->first();;
 
         if ($reservation) {
-            return Order::where('reservation_id', $reservation->id)
-                ->where('status', '!=', 'paid')->first();
+            return Order::where('reservation_id', $reservation->id)->first();
         }
         return Order::where('table_id', $table->id)
             ->where('status', '=', 'ordered')->first();
